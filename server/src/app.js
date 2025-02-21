f756ad5535ad81080552c8d5a959593082535e33
Arnav Kumar <144643023+arnavkumar2k5@users.noreply.github.com>
2025-02-21 23:08:10 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:8000",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
         allowedHeaders: "Content-Type,Authorization",
